73191d89cf677f20ab02f50f9860335b2b201681
chrialge <chrialge@gmail.com>
2024-02-23 15:02:52 +0100

    add structure markup dinamic

--- assest/js/script.js
+++ assest/js/script.js
@@ -1,1 +1,22 @@
 console.log("ciao")
+
+const listImage =[
+    "01.webp",
+    "02.webp",
+    "03.webp",
+    "04.webp",
+    "05.webp"
+]
+const slides = document.querySelector('.slides')
+let activeImage = 0
+
+for (let i = 0; i < listImage.length; i++) {
+    const slide = listImage[i];
+    console.log(slide)
+    const imageMarkup = `
+    <img class="${i == activeImage ? 'active' : '' }" src="./assest/img/${slide}" alt="">
+    `
+    console.log(imageMarkup);
+
+    slides.innerHTML += ('beforeend', imageMarkup)
+}
