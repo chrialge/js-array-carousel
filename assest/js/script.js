39410c1b07073a16e159874f4639f540aae12df9
chrialge <chrialge@gmail.com>
2024-02-24 17:55:33 +0100

    add another piece of bonus 2

--- assest/js/script.js
+++ assest/js/script.js
@@ -10,9 +10,15 @@ const listImage =[
 const slides = document.querySelector('.slides')
 let activeImage = 0
 
+const thumbnails = document.querySelector('.thumbnails')
+
 const prevElement = document.querySelector('.prev');
 const nextElement = document.querySelector('.next');
 
+const prevThumbnailsElement = document.querySelector('.prev-thumbnails')
+const nextThumbnailsElement = document.querySelector('.next-thumbnails')
+
+console.log(prevThumbnailsElement)
 
 for (let i = 0; i < listImage.length; i++) {
     const slide = listImage[i];
@@ -20,14 +26,32 @@ for (let i = 0; i < listImage.length; i++) {
     const imageMarkup = `
     <img class="${i == activeImage ? 'active' : '' } image-central" src="./assest/img/${slide}" alt="">
     `
-
-
+    const thumbnailsMarkup =`
+    <div class="card-thumbnails">
+        <img class="${i == activeImage ? 'current-image' : '' } image-thumbnails" src="./assest/img/${slide}" alt="">
+        <div class="container-overlay">
+            <div class="overlay"></div>
+        </div>
+    </div>
+    `
+    const buttonThumbnailsMarkup =`
+    <div class="actions">
+        <button class="prev-thumbnails">
+            <i class="fa-solid fa-chevron-up"></i>
+        </button>
+        <button class="next-thumbnails">
+            <i class="fa-solid fa-chevron-down"></i>
+        </button>
+    </div>
+    `
+    thumbnails.innerHTML += (thumbnailsMarkup)
+    thumbnails.innerHTML += (buttonThumbnailsMarkup)
     slides.innerHTML += (imageMarkup)
 }
 // constante array con dentro tutte le immagini
 
 const imageElement = document.querySelectorAll('.image-central')
-
+console.log(imageElement)
 prevElement.addEventListener('click', function(e) {
     e.preventDefault();
 
@@ -44,7 +68,7 @@ prevElement.addEventListener('click', function(e) {
 
 }
 )
-console.log(listImage.lenght)
+
 nextElement.addEventListener('click', function(e) {
     e.preventDefault();
 
@@ -59,3 +83,28 @@ nextElement.addEventListener('click', function(e) {
     imageElement[activeImage].classList.add('active');
 }
 )
+// bonus 2
+console.log(prevThumbnailsElement)
+
+const thumbnailsElement = document.querySelectorAll('.image-thumbnails')
+
+console.log(thumbnailsElement)
+
+nextThumbnailsElement.addEventListener('click', function(e) {
+    e.preventDefault();
+
+
+    activeImage++;
+    if(activeImage > imageElement.length - 1){
+        activeImage = 0;
+    }
+    const image = document.querySelector('img.current-image');
+    image.classList.remove('current-image');
+
+    thumbnailsElement[activeImage].classList.add('current-image');
+}
+)
+
+
+
+
